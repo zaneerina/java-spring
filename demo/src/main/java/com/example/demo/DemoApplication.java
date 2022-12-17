bd826a2e92aa80c260d0b4a5f956fa19ee3cb4a2
zaneerina <zane.erina@gmail.com>
2022-12-17 16:04:29 +0200

    created simple API using spring boot

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	// take a value from URL request and then map it as a parameter in the method
+//	public String hello ()
+	// user will be able to put in hello?name={some_name}
+	public String hello (@RequestParam(value = "name", defaultValue = "World")
+						 //then we need to define the datatype for this parameter & name the parameter:
+						 String name
+	){
+		return String.format("Hello %s!", name);
+
+	}
+}
+
